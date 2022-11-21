313501888b72ffe22ab80ce73876a8725c7a4f4b
Mollie Asbury <masbury@hawaii.edu>
2022-11-21 17:55:58 +1000

    update for fig2

--- R/figure2.R
+++ R/figure2.R
@@ -68,6 +68,9 @@ tog <- data.frame("true" = all_Ds,
                   "R_theory" = R_theory,
                   "H" = Height)
 write.csv(tog, "data/DfromSimulatedSurfaces.csv")
+###
+
+tog <- read.csv("data/DfromSimulatedSurfaces.csv")
 
 # least squared method to define plane
 head(tog)
@@ -87,18 +90,24 @@ for (r in 1:length(Rs)) {
     mdat$H <- log10(mdat$H / (L0 * sqrt(2)))
     mdat$m <- 1
     
-    # best fit orthogonal plane 
-    A <- data.matrix(mdat)
-    A <- matrix(c (sum(mdat$R * mdat$R) , sum(mdat$R * mdat$H) , sum(mdat$R) , 
-                   sum(mdat$R * mdat$H) , sum(mdat$H * mdat$H) , sum(mdat$H) ,
-                   sum(mdat$R) , sum(mdat$H) , nrow(mdat)), nrow = 3, ncol = 3)
-    A.i <- inv(A)
-    B <- matrix(c (sum(mdat$R * tog[,Ds[d]]) , 
-                   sum(mdat$H * tog[,Ds[d]]) , 
-                   sum(tog[,Ds[d]])), nrow = 3, ncol = 1)
-    fin <- A.i %*% B
+    # best fit least squares plane
+    A <- as.matrix(mdat)
+    B <- as.matrix(tog[c(paste0(Ds[d]))])
+    fin <- inv(t(A)%*%A)%*%t(A)%*%B
     
     f <- function(x, y) { z <- x * fin[1,1] + y * fin[2,1] + fin[3,1] }
+    
+    error <- B - A %*% fin # orthogonal distances 
+    rss <- sum((error - mean(0))^2) # == SSE
+    ssr <- sum((error - 0)^2)
+    
+    rmse <- sqrt(mean(error^2))
+    
+    assign(paste0("RMSE_",Rs[[r]], "_", Ds[[d]]),
+           sqrt(sum(error^2) / length(error)))
+    
+    assign(paste0("R2_",Rs[[r]], "_", Ds[[d]]), 
+           1 - (ssr/(rss+ssr)))
     
     # plane for plotting 
     assign(paste0("R.pred_", Rs[[r]]), seq(min(mdat$R), max(mdat$R), length=grid.lines))
@@ -110,11 +119,12 @@ for (r in 1:length(Rs)) {
            matrix(f(mat$R, mat$H), nrow = grid.lines, ncol = grid.lines))
     
     # d from plane vs data d 
-    comp <- data.frame("TrueD" = tog[,Ds[d]],
-                       "PlaneD" = f(mdat$R, mdat$H))
-    comp$R <- comp$TrueD - comp$PlaneD
-    assign(paste0("R2_",Rs[[r]], "_", Ds[[d]]), 
-           round(1 - ( sum((comp$TrueD - comp$PlaneD)^2) / (sum((comp$TrueD - mean(comp$TrueD))^2))),4)) }
+    # comp <- data.frame("TrueD" = tog[,Ds[d]],
+    #                    "PlaneD" = f(mdat$R, mdat$H))
+    # comp$R <- comp$TrueD - comp$PlaneD
+    # assign(paste0("R2_",Rs[[r]], "_", Ds[[d]]), 
+    #        round(1 - ( sum((comp$TrueD - comp$PlaneD)^2) / (sum((comp$TrueD - mean(comp$TrueD))^2))),4)) 
+    }
   }
 
 # Lizard island data 
@@ -130,17 +140,25 @@ for (r in 1:length(Rs)) {
   mdat$m <- 1
     
   # best fit orthogonal plane 
-  A <- data.matrix(mdat)
-  A <- matrix(c (sum(mdat$R * mdat$R) , sum(mdat$R * mdat$H) , sum(mdat$R) , 
-                 sum(mdat$R * mdat$H) , sum(mdat$H * mdat$H) , sum(mdat$H) ,
-                 sum(mdat$R) , sum(mdat$H) , nrow(mdat)), nrow = 3, ncol = 3)
-  A.i <- inv(A)
-  B <- matrix(c (sum(mdat$R * dta_rdh$D) , 
-                 sum(mdat$H * dta_rdh$D) , 
-                 sum(dta_rdh$D)), nrow = 3, ncol = 1)
-  fin <- A.i %*% B
+  A <- as.matrix(mdat)
+  B <- as.matrix(dta_rdh["D"])
+  fin <- inv(t(A)%*%A)%*%t(A)%*%B
     
   f <- function(x, y) { z <- x * fin[1,1] + y * fin[2,1] + fin[3,1] }
+  
+  error <- B - A %*% fin # orthogonal distances -- residuals 
+
+  rss <- sum((error - mean(0))^2) # == SSE
+  rse <- sqrt(rss/(length(error) - 2)) # residual standard error 
+  ssr <- sum((error - 0)^2)
+  
+  assign(paste0("R2_",Rs[[r]], "_lizard"), 
+         1 - (ssr/(rss+ssr)))
+  
+  assign(paste0("RMSE_",Rs[[r]], "_lizard"),
+         sqrt(sum(error^2) / length(error)))
+  
+  # mean(mdat$R) * fin[1,1] + mean(mdat$H) * fin[2,1] - mean(dta_rdh$D) #confirmation plane is going through centroid 
   
   # plane for plotting 
   assign(paste0("R.pred_", Rs[[r]], "_lizard"), seq(min(mdat$R), max(mdat$R), length=grid.lines))
@@ -152,11 +170,12 @@ for (r in 1:length(Rs)) {
          matrix(f(mat$R, mat$H), nrow = grid.lines, ncol = grid.lines))
     
   # d from plane vs data d 
-  comp <- data.frame("TrueD" = dta_rdh$D,
-                     "PlaneD" = f(mdat$R, mdat$H))
-  comp$R <- comp$TrueD - comp$PlaneD
-  assign(paste0("R2_",Rs[[r]], "_lizard"), 
-         round(1 - ( sum((comp$TrueD - comp$PlaneD)^2) / (sum((comp$TrueD - mean(comp$TrueD))^2))),4)) }
+  # comp <- data.frame("TrueD" = dta_rdh$D,
+  #                    "PlaneD" = f(mdat$R, mdat$H))
+  # comp$R <- comp$TrueD - comp$PlaneD
+  # assign(paste0("R2_",Rs[[r]], "_lizard"), 
+  #        round(1 - ( sum((comp$TrueD - comp$PlaneD)^2) / (sum((comp$TrueD - mean(comp$TrueD))^2))),4)) 
+  }
 
 # FIGURE 2
 pal <- fish(100, option = "Trimma_lantana")
@@ -176,12 +195,20 @@ scatter3D(log10(dta_rdh$R ^2 - 1), log10(dta_rdh$H / (L0 * sqrt(2))), dta_rdh$D,
           theta=215, 
           phi=0,
           colkey=FALSE)
-print(R2_R_lizard)
-text3D(-0.8, 0.8, 2.48, labels = expression(italic(r)^2 == 0.8714), surf = NULL, add = TRUE)
+# print(R2_R_lizard)
+# text3D(-0.8, 0.8, 2.48, labels = expression(italic(r)^2 == 0.8714), surf = NULL, add = TRUE)
+print(RMSE_R_lizard)
+text3D(-0.8, 0.8, 2.48, labels = expression(italic(RMSE) == 0.049), surf = NULL, add = TRUE)
 mtext(expression(bold("DEM Rugosity")), side = 3, line = -1, cex = 1)
 mtext(expression(bold("Torres-Pulliza")), side = 2, line= 1.5, cex = 1)
 mtext("A", side = 3, at = -.5, line = -.5)
 
+for (i in length(error) ) {
+  if (error[i] > 0) {
+    dta_rdh$Dplot <- dta_rdh$D + error[i]
+  } else {
+    dta_rdh$Dplot <- dta_rdh$D - error[i]
+}} ### still cant see because they are so small 
 scatter3D(log10(dta_rdh$R_theory ^2 - 1), log10(dta_rdh$H / (L0 * sqrt(2))), dta_rdh$D,
           pch = 20, 
           cex = .5,
@@ -189,7 +216,8 @@ scatter3D(log10(dta_rdh$R_theory ^2 - 1), log10(dta_rdh$H / (L0 * sqrt(2))), dta
           xlab = "Rugosity",
           ylab = "Height range",
           zlab = "Fractal dimension",
-          surf = list(x = R.pred_R_theory_lizard, y = H.pred_lizard, z = D.mat_R_theory_lizard, facets = NA, col = rgb(0,0,0,0.05), fitpoits = dta_rdh$D),
+          surf = list(x = R.pred_R_theory_lizard, y = H.pred_lizard, z = D.mat_R_theory_lizard, 
+                      facets = NA, col = rgb(0,0,0,0.05), fitpoits = dta_rdh$Dplot ),
           theta=215, 
           phi=0,
           colkey=FALSE)
